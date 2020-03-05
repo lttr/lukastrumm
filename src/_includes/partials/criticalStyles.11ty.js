ba83cd5ebac12f3a79f1b9281732e5fd8d2cae41
Lukas Trumm <lukas.trumm@centrum.cz>
2020-03-05 14:28:04 +0100

    Improve main font with unicode ranges

--- src/_includes/partials/criticalStyles.11ty.js
+++ src/_includes/partials/criticalStyles.11ty.js
@@ -4,6 +4,7 @@ const base64Font = inline('../../css/charter_regular-webfont_optimized')
 const fontUrl = `data:font/woff2;charset=utf-8;base64,${base64Font}`
 const indexCss = inline('../../css/index.css')
 
+// unicode-range: 0-9, A-Z, a-z
 module.exports = () => `
 <style>
 @font-face {
@@ -13,6 +14,7 @@ module.exports = () => `
   font-weight: normal;
   font-stretch: normal;
   font-display: swap;
+  unicode-range: U+0030-0039, U+0041-005A, U+0061-007A;
 }
 ${indexCss}
 </style>
