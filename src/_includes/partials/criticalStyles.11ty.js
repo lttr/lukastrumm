d99f244cd03b8f1eb669c72d933a9321940f505f
Lukas Trumm <lukas.trumm@centrum.cz>
2020-03-25 08:36:06 +0100

    Rename main styles file

--- src/_includes/partials/criticalStyles.11ty.js
+++ src/_includes/partials/criticalStyles.11ty.js
@@ -2,7 +2,7 @@ const inline = require('../helpers/inline')
 
 const base64Font = inline('../../css/charter_regular-webfont_optimized')
 const fontUrl = `data:font/woff2;charset=utf-8;base64,${base64Font}`
-const indexCss = inline('../../css/index.css')
+const indexCss = inline('../../css/styles.css')
 
 // unicode-range: 0-9, A-Z, a-z
 module.exports = () => `
