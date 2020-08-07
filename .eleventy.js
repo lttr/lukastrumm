0de29017a517a7af61c14af1e4a6250d8a98a1b5
Lukas Trumm <lukas.trumm@centrum.cz>
2020-08-07 15:17:44 +0200

    Remove labs from the feed collection

--- .eleventy.js
+++ .eleventy.js
@@ -48,7 +48,7 @@ module.exports = function (eleventyConfig) {
   )
   eleventyConfig.addCollection(
     'feed',
-    collectionByFolders('articles/', 'blog/', 'labs/')
+    collectionByFolders('articles/', 'blog/')
   )
   eleventyConfig.addCollection(
     'posts',
