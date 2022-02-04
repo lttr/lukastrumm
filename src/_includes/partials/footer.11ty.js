9439486309d4809e50361bb6430c5a4a7650215c
Lukas Trumm <lukas.trumm@gmail.com>
2022-02-04 20:43:18 +0100

    Fix icons

--- src/_includes/partials/footer.11ty.js
+++ src/_includes/partials/footer.11ty.js
@@ -8,19 +8,19 @@ module.exports = () =>
   html`
     <footer class="page-footer">
       <a href="https://github.com/lttr" aria-label="Github - lttr">
-        <span class="social-icon">${raw`${siGithub}`}</span>
+        <span class="social-icon">${raw`${siGithub.svg}`}</span>
       </a>
       <a
         href="https://www.goodreads.com/user/show/64207622-luk-trumm"
         aria-label="Goodreads - lukastrumm"
       >
-        <span class="social-icon">${raw`${siGoodreads}`}</span>
+        <span class="social-icon">${raw`${siGoodreads.svg}`}</span>
       </a>
       <a
         href="https://twitter.com/lukastrumm"
         aria-label="Twitter - lukastrumm"
       >
-        <span class="social-icon">${raw`${siTwitter}`}</span>
+        <span class="social-icon">${raw`${siTwitter.svg}`}</span>
       </a>
       <span class="copyright">
         Lukas Trumm &copy; ${new Date().getFullYear()}
